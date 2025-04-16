37db6d78cf24e4a57ab32e30c691281cc29066d4
am1t-kumar <thorragnarak@gmail.com>
2025-04-16 08:25:03 +0530

    add: minor changes

--- src/features/linkedlist/index.ts
+++ src/features/linkedlist/index.ts
@@ -1,3 +1,8 @@
+interface IPushAt<T> {
+  index: number;
+  value: T;
+}
+
 class Node<T> {
   public data: T;
   public next: Node<T> | null;
@@ -29,26 +34,87 @@ class LinkedList<T> {
     current.next = node;
   }
 
+  prepend(value: T) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      return (this.head = node);
+    }
+
+    node.next = this.head;
+    this.head = node;
+  }
+
   printList() {
     if (!this.head) {
-      return console.log("No Length");
+      return console.log("EMPTY LIST");
     }
 
     let current = this.head;
-    let answer = "";
+    let ans = "";
 
-    while (current !== null) {
-      answer += `${current.data} ->`;
+    while (current) {
+      ans += `${current.data} =>`;
       current = current.next as Node<T>;
     }
 
-    console.log("Print List", answer);
+    console.log("LIST: ", ans);
+    return ans;
+  }
+
+  size() {
+    let size = 0;
+    if (!this.head) {
+      return size;
+    }
+
+    let current = this.head;
+    while (current) {
+      size = size + 1;
+      current = current.next as Node<T>;
+    }
+
+    console.log("SIZE: ", size);
+    return size;
+  }
+
+  clear() {
+    return (this.head = null);
+  }
+
+  pushat(index: IPushAt<T>["index"], value: IPushAt<T>["value"]) {
+    const currentSize = this.size();
+    if (index > currentSize) {
+      return console.log("OVERFLOW");
+    }
+
+    if (!this.head || index === 0) {
+      return console.log("NO PLACE TO INSET NODE");
+    }
+
+    let size = 0;
+    let current = this.head;
+
+    while (size !== index) {
+      const nextIndex = size + 1;
+      if (nextIndex === index) {
+        const node = new Node(value);
+        node.next = current.next;
+        current.next = node;
+      }
+      current = current.next as Node<T>;
+      size++;
+    }
   }
 }
 
 const LLinkedList = new LinkedList();
 console.log(LLinkedList.printList());
-console.log(LLinkedList.append(2));
-console.log(LLinkedList.append(3));
-console.log(LLinkedList.append(4));
+console.log(LLinkedList.append(1));
+console.log(LLinkedList.printList());
+console.log(LLinkedList.printList());
+console.log(LLinkedList.size());
+console.log(LLinkedList.pushat(1, 2));
+console.log(LLinkedList.pushat(1, 3));
+console.log(LLinkedList.pushat(2, 4));
 console.log(LLinkedList.printList());
